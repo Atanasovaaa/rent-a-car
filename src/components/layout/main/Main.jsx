e63d5e8a5824e3707e8ccc687ceec27def2220a8
dani <atanasova.yordankaa@gmail.com>
2021-09-13 13:38:54 +0300

    Main works, header/footer done

--- src/components/layout/main/Main.jsx
+++ src/components/layout/main/Main.jsx
@@ -3,7 +3,7 @@ import "./_main.scss";
 export default function Main() {
     return (
         <div className="main-content">
-            <h2>Test test</h2>
+            Main works!
         </div>
     );
 }
